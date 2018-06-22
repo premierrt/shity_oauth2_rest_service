10f8fc2db7af1dbda678d0c08d7c6b4a4a9abd81
Rafał <rafal@xxx.xx>
2018-06-22 16:13:40 +0200

    2222

--- src/main/java/oauth2_spring_boot/Oauth2SpringBootApplication.java
+++ src/main/java/oauth2_spring_boot/Oauth2SpringBootApplication.java
@@ -2,14 +2,19 @@ package oauth2_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
 
-public class Oauth2SpringBootApplication {
+public class Oauth2SpringBootApplication extends AuthorizationServerConfigurerAdapter{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2SpringBootApplication.class, args);
 	}
+
+	
+	
 }
--- src/main/java/oauth2_spring_boot/rest_service/GreetingController.java
+++ src/main/java/oauth2_spring_boot/rest_service/GreetingController.java
@@ -3,18 +3,20 @@ package oauth2_spring_boot.rest_service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RestController
 public class GreetingController {
 	
 	
 @Autowired	
 private GreetingService greetingService;	
-
+@PreAuthorize("#oauth2.hasScope('level_1')")
 @RequestMapping(value="/greeting", method=RequestMethod.GET)
 public ResponseEntity<String> greet (){
 	return new ResponseEntity<String>(greetingService.greet(), HttpStatus.OK );
